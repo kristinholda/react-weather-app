c6eb0e7cd6feb3d932fb644a9607c3fc1b13de26
kristinholda <k.holda@yahoo.com>
2023-12-13 14:14:37 -0500

    Added structure and style of app to build

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Current() {
+  return (
+    <div>
+      <small class="source">
+        <a
+          href="https://github.com/kristinholda/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        by Kristin Holda
+      </small>
+    </div>
+  );
+}
